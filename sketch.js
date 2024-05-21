6314e714caae1875ff8fc4abf123f621583a3108
Dkingnoprime <diudestroi@gmail.com>
2024-05-21 17:59:45 -0300

    trex atualizado

--- sketch.js
+++ sketch.js
@@ -1,47 +1,53 @@
 var trex, trex_running, edges;
 var groundImage;
 
-function preload(){
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+function preload() {
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
   groundImage = loadImage("ground2.png")
 }
 
-function setup(){
-  createCanvas(600,200);
-  
+function setup() {
+  createCanvas(600, 200);
+
   //criando o trex
-  trex = createSprite(50,160,20,50);
+  trex = createSprite(50, 160, 20, 50);
   trex.addAnimation("running", trex_running);
   edges = createEdgeSprites();
 
-  ground=createSprite (200,190,800,20);
+  ground = createSprite(200, 180, 800, 20);
   ground.addImage(groundImage)
-  
+
+  //chão invisível 
+  invisibleGround = createSprite(200, 190, 400, 10);
+  invisibleGround.visible = false;
+
   //adicione dimensão e posição ao trex
   trex.scale = 0.5;
   trex.x = 50
 }
 
 
-function draw(){
+function draw() {
   //definir a cor do plano de fundo 
   background(220);
   //movimento do chão
-  ground.velocityX=-3
-  if (ground.x<0) {
-    ground.x=ground.width/2
+  ground.velocityX = -3
+  if (ground.x < 0) {
+    ground.x = ground.width / 2
   }
   //registrando a posição y do trex
   console.log(trex.y)
-  
+
   //pular quando tecla de espaço for pressionada
-  if(keyDown("space")){
+  if (keyDown("space") && trex.y >= 155) {
     trex.velocityY = -10;
+
   }
-  
+
   trex.velocityY = trex.velocityY + 0.5;
-  
- //impedir que o trex caia
-   trex.collide(ground)
+
+  //impedir que o trex caia
+  trex.collide(invisibleGround)
   drawSprites();
-}  
+}
+ 
